6cec58efcdb6f8838ff4fccdb3c9cdf5f941bfeb
Akshar Raaj <akshar@urbanpiper.com>
2024-08-06 21:49:45 +0530

    Singleton Pattern: Added a method to execute queries

--- singleton-pattern/database.py
+++ singleton-pattern/database.py
@@ -2,10 +2,15 @@ class Database(object):
     _connection = None
 
     def __init__(self):
+        self.num_queries = 0
         if Database._connection is not None:
             raise Exception("Database already initialized")
         else:
             Database._connection = self
+
+    def execute_query(self, query):
+        self.num_queries += 1
+        print("Executing query")
 
     @staticmethod
     def get_connection():
